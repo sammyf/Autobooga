5183cdd584ccc6d6d802918d8e4fe3c576472b8e
Sammy Fischer <sammy@cosmic-bandito.com>
2023-07-06 16:51:57 +0200

    search for meta in case the page is empty.

--- script.py
+++ script.py
@@ -12,8 +12,8 @@
 # 
 # You should have received a copy of the GNU General Public License along with this program. 
 # If not, see <https://www.gnu.org/licenses/>.
-# 
-
+#
+import os.path
 import string
 import requests
 import json
@@ -23,15 +23,16 @@ from modules import chat
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import re
+import sqlite3
 
 ############# OPTIONS #############
 # You should review the values in the following section and edit
 # them according to your needs.
 #
 ## searx instance for internet searches
-SEARX_SERVER = 'ENTER A SEARX INSTANCE HERE!'
+SEARX_SERVER = 'https://searx.cosmic-bandito.com'
 ## Maximum tokens to return when retrieving a webpage (1 token is about 1 word)
-MAX_EXTRACTED_TEXT = 1000
+MAX_EXTRACTED_TEXT = 1500
 ## Maximum entries to return when doing an internet search
 MAX_SEARCH_RESULTS = 5
 ## Maximum tokens to return when doing an internet search
@@ -39,15 +40,41 @@ MAX_SEARCH_TEXT = 1500
 #
 ############# END OF OPTIONS #############
 
+############# TRIGGER PHRASES  #############
+## you can add anything you like here, just be careful not to trigger unwanted searches or even loops
 INTERNET_QUERY_PROMPTS=[ "search the internet for information on", "search the internet for information about",
                          "search for information about", "search for information on", "search for ",
                          "i need more information on ", "search the internet for ",
                          "can you provide me with more specific details on ",
+                         "can you provide me with details on ",
+                         "can you provide me with more details on ",
+                         "can you provide me with more specific details about ",
+                         "can you provide me with details about ",
+                         "can you provide me with more details about ",                                                  
                          "what can you find out about ", "what information can you find out about ",
                          "what can you find out on ", "what information can you find out on ",
                          "what can you tell me about ", "what do you know about ",  "ask the search engine on ",
-                         "ask the search engine about "]
+                         "ask the search engine about ", "ask the seach engine on "]
 
+DBNAME = ""
+character  = ""
+
+# If 'state' is True, will hijack the next chat generation
+input_hijack = {
+    'state': False,
+    'value': ["", ""]
+}
+
+def generate_db(character):
+    if not os.path.isfile(f"mtm_{character}.db"):
+        c = sqlite3.connect(f"mtm_{character}.db")
+        c.execute('CREATE TABLE "mtm" (	'
+                  '"id"	INTEGER NOT NULL UNIQUE,	'
+                  '"paragraph"	INTEGER,	'
+                  '"content"	TEXT,	'
+                  '"summary"	TEXT,	'
+                  'PRIMARY KEY("id" AUTOINCREMENT));')
+        c.close()
 
 def call_searx_api(query):
     url = f"{SEARX_SERVER}?q={query}&format=json"
@@ -87,14 +114,32 @@ def call_searx_api(query):
 
 ## returns only the first URL in a prompt
 def extract_url(prompt):
+    print("entering extract URL\n")
     url=""
     # Regular expression to match URLs
     url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     # Find all URLs in the text
-    urls = re.findall(url_pattern, prompt)
+    urls = re.findall(url_pattern, prompt.lower())
     if len(urls)>0:
         url=urls[0]
+    print("\n******* URL FOUND : '"+url+"' *********\n")
     return url
+
+def set_paragraph_summary(paragraph:int,content:string, summary:string):
+    global character
+    c = sqlite3.connect(f"mtm_{character}.db")
+    c.conn.cursor()
+    c.execute(f"INSERT INTO mtm VALUES ({paragraph}, '{content}', '{summary}')")
+    c.close()
+
+def get_paragraph_summary(paragraph):
+    c = sqlite3.connect(f"mtm_{character}.db")
+    c.conn.cursor()
+    rs = ""
+    for row in c.execute(f'SELECT summary FROM mtm WHERE paragraph={paragraph}'):
+        rs = rs + row
+    c.close()
+    return rs
 
 def trim_to_x_words(prompt:string, limit:int):
     rev_rs = []
@@ -138,20 +183,41 @@ def extract_query(prompt):
             rs[1] += rest
     return rs
 
-def output_modifier(llm_response):
+def get_page(url, prompt):
+    text = "This web page doesn't have any useable content. Sorry."
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.find_all('p')
+    if len(paragraphs) > 0:
+        text = '\n'.join(p.get_text() for p in paragraphs)
+    else:
+        text = f"This web page doesn't seem to have any readable content."
+        metas = soup.find_all("meta")
+        for m in metas:
+            if 'content' in m.attrs:
+                if m['name'] == 'page-topic' or m['name'] == 'description':
+                    text += f"It's {m['name']} is '{m['content']}'"
+    print("******* TEXT FOUND : \n"+text+"\n *********\n")
+    if prompt == url:
+        prompt = f"Summarize the content from this url : {url}"
+    prompt = f"{prompt}\nContent of {url} : \n{trim_to_x_words(text, MAX_EXTRACTED_TEXT)}[...]\n"
+    return prompt
+
+def output_modifier(llm_response, state):
+    global character
     # print("original response : "+llm_response)
     # If the LLM needs more information, we call the SEARX API.
     q = extract_query(llm_response)
     if q[0] != "":
-        input_highjack = {
-            'state':True,
-            'value':[f"\nsearch for "+q[0]+".", f"Searching the internet for information on '{q[0]}' ..."]
-        }
+        input_hijack.update({'state':True,'value':[f"\nsearch for "+q[0], f"Searching the internet for information on '{q[0]}' ..."]})
         ## this is needed to avoid a death loop.
         llm_response = f"I'll ask the search engine on {q[0]} ..."
     return llm_response
 
-def input_modifier(prompt):
+def input_modifier(prompt, state):
+    global character
+    if character == "":
+        character = state["character_menu"]
     now = "it is " + datetime.now().strftime("%H:%M on %A %B,%d %Y") + "."
 
     q = extract_query(prompt)
@@ -166,9 +232,5 @@ def input_modifier(prompt):
     else:
         url = extract_url(prompt)
         if url != "":
-            response = requests.get(url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-            paragraphs = soup.find_all('p')
-            text = '\n'.join(p.get_text() for p in paragraphs)
-            prompt = "Content of the following url : \n"+trim_to_x_words(text, MAX_EXTRACTED_TEXT)+"..."+prompt
+            prompt = get_page(url, prompt)
     return now+"\n"+prompt
