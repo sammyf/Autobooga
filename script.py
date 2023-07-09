7d64a1d7fd20ad3ccffe8dc6f4de55ba72eb43ef
Sammy Fischer <sammy@cosmic-bandito.com>
2023-07-09 11:36:07 +0200

    Bad merge corrected

--- script.py
+++ script.py
@@ -24,9 +24,9 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import re
 import gradio as gr
+from PyPDF2 import PdfReader
 
 CONFIG_FILE="extensions/autobooga/autobooga_config.json"
-
 ############# TRIGGER PHRASES  #############
 ## you can add anything you like here, just be careful not to trigger unwanted searches or even loops
 INTERNET_QUERY_PROMPTS=[ "search the internet for information on", "search the internet for information about",
@@ -43,6 +43,13 @@ INTERNET_QUERY_PROMPTS=[ "search the internet for information on", "search the i
                          "what can you tell me about ", "what do you know about ",  "ask the search engine on ",
                          "ask the search engine about ", "ask the seach engine on "]
 
+FILE_QUERY_PROMPTS=[
+    "open the file ",
+    "read the file ",
+    "summarize the file ",
+    "get the file "
+]
+
 DBNAME = ""
 character  = ""
 
@@ -52,29 +59,56 @@ input_hijack = {
     'value': ["", ""]
 }
 
-with open(CONFIG_FILE) as f:
-    config = json.load(f)
+def write_config():
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(params, f, indent=4)
+
+config = []
+try:
+    with open(CONFIG_FILE) as f:
+        config = json.load(f)
+except:
+    config = []
 
 params = {
-    "searx_server":config['searx_server'],
-    "max_search_results":config['max_search_results'],
-    "max_text_length":config['max_text_length']
+    "searx_server":"enter the url to a searx server capable of json here.",
+    "max_search_results":5,
+    "max_text_length":1000
 }
+
+if 'searx_server' in config:
+    params.update({"searx_server":config['searx_server']})
+if 'max_search_results' in config:
+    try:
+        params.update({"max_search_results":int(config['max_search_results'])})
+    except:
+        pass
+if 'max_text_length' in config:
+    try:
+        params.update({"max_text_length":int(config['max_text_length'])})
+    except:
+        pass
+
+write_config()
 
 def set_searx_server( x):
     params.update({"searx_server": x})
-    with open('extensions/moztts/tts_config.json', 'w') as f:
-        json.dump(params, f, indent=4)
+    write_config()
 
 def set_max_search_results( x):
-    params.update({"maximum_search_results": x})
-    with open(CONFIG_FILE, 'w') as f:
-        json.dump(params, f, indent=4)
+    try:
+        params.update({"max_search_results": int(x)})
+    except:
+        pass
+    write_config()
+
 
 def set_max_extracted_text(x):
-    params.update({"maximum_text_length": x})
-    with open(CONFIG_FILE, 'w') as f:
-        json.dump(params, f, indent=4)
+    try:
+        params.update({"max_text_length": int(x)})
+    except:
+        pass
+    write_config()
 
 
 def call_searx_api(query):
@@ -117,7 +151,6 @@ def call_searx_api(query):
 
 ## returns only the first URL in a prompt
 def extract_url(prompt):
-    print("entering extract URL\n")
     url=""
     # Regular expression to match URLs
     url_pattern = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
@@ -125,9 +158,7 @@ def extract_url(prompt):
     urls = re.findall(url_pattern, prompt.lower())
     if len(urls)>0:
         url=urls[0]
-    print("\n******* URL FOUND : '"+url+"' *********\n")
     return url
-
 
 def trim_to_x_words(prompt:string, limit:int):
     rev_rs = []
@@ -171,6 +202,22 @@ def extract_query(prompt):
             rs[1] += rest
     return rs
 
+def extract_file_name( prompt):
+    rs=""
+    # Join the terminators into a single string, separating each with a pipe (|), which means "or" in regex
+    open_prompt = ""
+    for qry in FILE_QUERY_PROMPTS:
+        if qry in prompt.lower():
+            open_prompt = qry
+            break
+    if open_prompt != "":
+        query_raw = prompt.lower().split(open_prompt)[1]
+        pattern = r"([\"'])(.*?)\1"
+        query = re.search(pattern, query_raw)
+        if query is not None:
+            rs = query.group(2)
+    return rs
+
 def get_page(url, prompt):
     text = "This web page doesn't have any useable content. Sorry."
     response = requests.get(url)
@@ -178,18 +225,53 @@ def get_page(url, prompt):
     paragraphs = soup.find_all('p')
     if len(paragraphs) > 0:
         text = '\n'.join(p.get_text() for p in paragraphs)
+        text = f"Content of {url} : \n{trim_to_x_words(text, params['max_text_length'])}[...]\n"
     else:
         text = f"This web page doesn't seem to have any readable content."
         metas = soup.find_all("meta")
         for m in metas:
             if 'content' in m.attrs:
                 if m['name'] == 'page-topic' or m['name'] == 'description':
-                    text += f"It's {m['name']} is '{m['content']}'"
-    if prompt == url:
-        prompt = f"Summarize the content from this url : {url}"
-    prompt = f"{prompt}\nContent of {url} : \n{trim_to_x_words(text, params['max_text_length'])}[...]\n"
+                    if m['content'] != None:
+                        text += f"It's {m['name']} is '{m['content']}'"
+    if prompt.trim() == url:
+        text = f"Summarize the content from this url : {url}"
+    return text
 
-    return prompt
+def read_pdf( fname):
+    parts = []
+
+    def visitor_body(text, cm, tm, fontDict, fontSize):
+        y = tm[5]
+        if y > 50 and y < 720:
+            parts.append(text)
+
+    pdf = PdfReader(fname)
+    rs = ""
+    for page in pdf.pages:
+        page = pdf.pages[5]
+        page.extract_text(visitor_text=visitor_body)
+        text_body = "".join(parts)
+        text_body = text_body.replace("\n", "")
+        rs += text_body+"\n"
+        if rs != trim_to_x_words(rs, params['max_text_length']):
+            break
+    return rs
+
+def open_file(fname):
+    rs = ""
+    print(f"Reading {fname}")
+    if fname.lower().endswith(".pdf"):
+        rs = read_pdf(fname)
+    else:
+        try:
+            with open(fname, 'w') as f:
+                lines = f.readlines()
+        except:
+            return "The file can not be opened. Perhaps the filename is wrong?"
+        rs = "\n".join(lines)
+    rs = trim_to_x_words(rs, params['max_text_length'] )
+    return f"This is the content of the file '{fname}':\n{rs}"
 
 def output_modifier(llm_response, state):
     global character
@@ -204,21 +286,26 @@ def output_modifier(llm_response, state):
 
 def input_modifier(prompt, state):
     global character
+
     if character == "":
         character = state["character_menu"]
     now = "it is " + datetime.now().strftime("%H:%M on %A %B,%d %Y") + "."
 
+    url = extract_url(prompt)
     q = extract_query(prompt)
-    if q[0] != "":
+    fn = extract_file_name(prompt)
+    print(f"Filename found : '{fn}'\nQuery found : {q[0]}\nUrl found : {url}\n")
+    if fn != "":
+        prompt = open_file(fn)+prompt
+    elif url != "":
+            prompt = get_page(url, prompt)+prompt
+    elif q[0] != "":
         searx_results = call_searx_api(q[0])
         # Pass the SEARX results back to the LLM.
         if(q[1] == ""):
             q[1] = "Summarize the results."
-        prompt = prompt + "." + searx_results+"."+q[1]
-    else:
-        url = extract_url(prompt)
-        if url != "":
-            prompt = get_page(url, prompt)
+        prompt = prompt + "\n" + searx_results+"."+q[1]
+
     return now+"\n"+prompt
 
 def ui():
@@ -228,8 +315,7 @@ def ui():
         with gr.Row():
             max_search_results = gr.Textbox(value=params['max_search_results'], label='The amount of search results to read.')
         with gr.Row():
-            max_extracted_text = gr.Text(value=params['max_text_length'], label='The maximum amount of words to read. Anything after that is truncated')
-
-    searx_server.change(lambda s: set_searx_server(s), searx_server, None),
-    max_search_results.change(lambda s: set_max_search_results(s), max_search_results, None),
-    max_extracted_text.change(lambda s: set_max_extracted_text(s), max_extracted_text, None),
+            max_extracted_text = gr.Textbox(value=params['max_text_length'], label='The maximum amount of words to read. Anything after that is truncated')
+    searx_server.change(lambda x: set_searx_server(x), searx_server, None)
+    max_search_results.change(lambda x: set_max_search_results(x), max_search_results, None)
+    max_extracted_text.change(lambda x: set_max_extracted_text(x), max_extracted_text, None)
